840d35c59da28bcfc94f01f0842d6d0c47a3829a
c4lyp5o <calypso.hellhammer@gmail.com>
2022-04-12 10:13:45 +0800

    changed joining msg, added userlist

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // calling friends
-import { createServer } from 'http';
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from "socket.io";
 
 //init app
 const app = express();
@@ -24,7 +25,7 @@ function normalizePort(val) {
     return false;
 }
 
-const port = normalizePort('8001');
+const port = normalizePort('8000');
 
 // error handler
 function errorHandler(error) {
@@ -54,27 +55,26 @@ server.on('error', errorHandler);
 server.on('listening', () => {
     const address = server.address();
     const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log(`Currently istening on ${bind}. Lessgo!`);
+    console.log(`Currently listening on ${bind}. Lessgo!`);
 });
 
 // configure socket.io
-import { Server } from "socket.io";
-const io = new Server({
+const io = new Server(server, {
     cors: {
         origin: '*',
     }
 });
 
 io.on('connection', (socket) => {
-    console.log(`Client ${socket.id} connected. Total clients: ${io.engine.clientsCount}. Total users: ${usercount}. Users: ${userlist}`);
+    console.log(`Client ${socket.id} connected. Total clients: ${io.engine.clientsCount}.`);
 
     socket.on('joining msg', (username) => {
         socket.nickname = username;
-        userlist.push(username);
+        userlist.push(socket.nickname);
         usercount++;
-        io.emit('chat message', `---${socket.nickname} joined the chat---`);
+        io.emit('chat message', `-- ${socket.nickname} joined the chat --`);
         io.emit('chat message', `There are ${usercount} users in the chat`);
-        // io.emit('chat message', `Users: ${userlist}`);
+        io.emit('chat message', `Users: ${userlist}`);
     });
     
     socket.on('disconnect', () => {
@@ -82,8 +82,9 @@ io.on('connection', (socket) => {
         userlist.splice(userlist.indexOf(socket.nickname), 1);
         usercount--;
         // console.log(`Client ${socket.id} disconnected`);
-        io.emit('chat message', `---${socket.nickname} left the chat---`);
-        io.emit('chat message', `There are ${usercount} users in the chat`);  
+        io.emit('chat message', `-- ${socket.nickname} left the chat --`);
+        io.emit('chat message', `There are ${usercount} users in the chat`);
+        io.emit('chat message', `Users: ${userlist}`);  
     });
 
     socket.on('chat message', (msg) => {
